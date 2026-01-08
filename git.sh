a87cfe766aaed88a15d0964c31ce1459275d0ffa
arshpreetjava <arshpreetjava@gmail.com>
2026-01-08 11:48:37 +0530

    Initalized chatbot project

---
+++ git.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [-z "$*"]; then
+  echo "❌ Please provide a commit message"
+  echo "Usage: ./git.sh \"your commit message\""
+  exit 1
+fi
+
+echo "Phase: Staging"
+git add .
+
+echo "Phase: Commiting"
+git commit -m "$*"
+
+echo "Phase: Push"
+git push
