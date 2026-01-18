9671de7cf738f5d9422d347b1db22dfce7521081
lakshaybxt <bishtlakshay14@gmail.com>
2026-01-18 16:30:54 +0530

    Updated

--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -6,32 +6,9 @@ import './App.css'
 
 function App() {
 	
-	const array = useState([
-		/*{
-		message: 'hello chatbot',
-		sender: 'user',
-		id: 'id1'
-	},
-	{
-		message: 'Hello! How can I help you?',
-		sender: 'robot',
-		id: 'id2'
-	},
-	{
-		message: `Can you get me today's date?`,
-		sender: 'user',
-		id: 'id3'
-	},
-	{
-		message: 'Today is January 8',
-		sender: 'robot',
-		id: 'id4'
-	}*/
-]);
+	const array = useState([]);
 
 	const [chatMessages, setChatMessages] = array;
-	// const chatMessages = array[0];
-	// const setChatMessages = array[1];
 	
 
 return(
