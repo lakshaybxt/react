82fa509a6d32fcbd8dea992be83c09f0e78ebe8d
arshpreetjava <arshpreetjava@gmail.com>
2026-01-19 13:20:37 +0530

    Added Login Form

---
+++ login-form/src/App.jsx
@@ -0,0 +1,16 @@
+import LoginForm from '../../project-chatbot/src/LoginForm';
+import './App.css'
+
+function App() {
+
+  return(
+    <>
+      <p>Hello, welcome to my website</p>
+      <LoginForm/>
+    </>
+  );
+}
+
+
+
+export default App
