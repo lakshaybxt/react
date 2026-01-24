005c54f34062e6424b0fc8517681b3b43b15fe4a
lakshaybxt <bishtlakshay14@gmail.com>
2026-01-24 16:57:26 +0000

    Moved order and tracking page to React

--- project-ecom/src/App.jsx
+++ project-ecom/src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router'
 import { useState } from 'react'
 import HomePage from './pages/HomePage'
 import CheckoutPage from './pages/CheckoutPage'
+import OrderPage from './pages/OrderPage'
+import TrackingPage from './pages/TrackingPage'
 import './App.css'
 
 function App() {
@@ -11,8 +13,10 @@ function App() {
     <>
       <Routes>
         <Route index element={<HomePage/>}/>
-        <Route path="/checkout" element={<CheckoutPage/>}></Route>
-        <Route path="/health" element={<p>200OK</p>}></Route>
+        <Route path="/checkout" element={<CheckoutPage/>}/>
+        <Route path="/health" element={<p>200OK</p>}/>
+        <Route path="/orders" element={<OrderPage/>}/>
+        <Route path="/tracking" element={<TrackingPage/>}/>
       </Routes>
       
     </>
